1fe9b5f695ae8d771a79696836273482a9116a60
Soares Chen <quiverjs@soareschen.com>
2013-04-14 02:59:18 +0200

    First commit for quiver-subrequest.

---
+++ README.md
@@ -0,0 +1,5 @@
+
+quiver-subrequest
+=================
+
+Quickly issue a client HTTP request to other HTTP servers
---
+++ test/test.js
@@ -0,0 +1,80 @@
+
+var http = require('http')
+var should = require('should')
+var subrequest = require('../lib/subrequest')
+var nodeStream = require('quiver-node-stream')
+var streamConvert = require('quiver-stream-convert')
+
+var testPort = 8005
+
+var createMockServer = function(port) {
+  var handler = function(request, response) {
+    var method = request.method
+    var path = request.url
+
+    var writeResponse = function(content) {
+      response.writeHead(200)
+      response.write(content)
+      response.end()
+    }
+
+    if(method == 'GET') {
+      path.should.equal('/get-path')
+      writeResponse('get response')
+    } else if(method == 'POST') {
+      path.should.equal('/post-path')
+      var requestStream = nodeStream.createNodeReadStreamAdapter(request)
+      streamConvert.streamToText(requestStream, function(err, requestText) {
+        should.not.exist(err)
+        requestText.should.equal('post content')
+
+        writeResponse('post response')
+      })
+    } else {
+      path.should.equal('/other-path')
+      writeResponse('other response')
+    }
+  }
+
+  var server = http.createServer(handler)
+  server.listen(port)
+}
+
+createMockServer(testPort)
+
+describe('test subrequest', function() {
+  it('test post request', function(callback) {
+    var requestHead = {
+      host: 'localhost',
+      port: testPort,
+      method: 'POST',
+      path: '/post-path'
+    }
+
+    var requestBody = streamConvert.textToStream('post content')
+    subrequest.subrequestToStream(requestHead, requestBody, function(err, responseStream) {
+      should.not.exist(err)
+
+      streamConvert.streamToText(responseStream, function(err, responseText) {
+        should.not.exist(err)
+
+        responseText.should.equal('post response')
+        callback()
+      })
+    })
+  })
+
+  it('test get request', function(callback) {
+    var url = 'http://localhost:8005/get-path'
+    subrequest.getRequestToStream(url, function(err, responseStream) {
+      should.not.exist(err)
+
+      streamConvert.streamToText(responseStream, function(err, responseText) {
+        should.not.exist(err)
+
+        responseText.should.equal('get response')
+        callback()
+      })
+    })
+  })
+})
